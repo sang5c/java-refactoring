255ca95ef168f0946714c8f022d163fd382bbac0
sang5c <skah321@gmail.com>
2022-11-02 11:13:21 +0900

    임시 변수를 메소드 호출로 변환 기법

--- src/main/java/com/example/javarefactoring/ch01/Rental.java
+++ src/main/java/com/example/javarefactoring/ch01/Rental.java
@@ -9,6 +9,27 @@ public class Rental {
         _daysRented = daysRented;
     }
 
+    public double getCharge() {
+        double result = 0;
+        // 비디오 종류별 대여료 계산
+        switch (getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                result += 2;
+                if (getDaysRented() > 2)
+                    result += (getDaysRented() - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                result += getDaysRented() * 3;
+                break;
+            case Movie.CHILDRENS:
+                result += 1.5;
+                if (getDaysRented() > 3)
+                    result += (getDaysRented() - 3) * 1.5;
+                break;
+        }
+        return result;
+    }
+
     public int getDaysRented() {
         return _daysRented;
     }
---
+++ src/test/java/com/example/javarefactoring/ch01/CustomerTest.java
@@ -0,0 +1,28 @@
+package com.example.javarefactoring.ch01;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomerTest {
+
+    @Test
+    void statement() {
+        Customer customer = new Customer("홍길동");
+        System.out.println(customer.statement());
+
+        Movie movie1 = new Movie("아바타", Movie.NEW_RELEASE);
+        Movie movie2 = new Movie("어벤져스", Movie.REGULAR);
+        Movie movie3 = new Movie("토이스토리", Movie.CHILDRENS);
+
+        Rental rental1 = new Rental(movie1, 3);
+        Rental rental2 = new Rental(movie2, 2);
+        Rental rental3 = new Rental(movie3, 5);
+
+        customer.addRental(rental1);
+        customer.addRental(rental2);
+        customer.addRental(rental3);
+
+        System.out.println(customer.statement());
+    }
+}
