76b89c7bf7c37b266c44d2af0a4d633b12567cf0
sang5c <skah321@gmail.com>
2022-11-02 10:04:43 +0900

    first commit

---
+++ src/main/java/com/example/javarefactoring/JavaRefactoringApplication.java
@@ -0,0 +1,13 @@
+package com.example.javarefactoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaRefactoringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaRefactoringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javarefactoring/JavaRefactoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javarefactoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaRefactoringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
